ce5c610ce1780e6f1f535485b5269e357bbaad8c
turingsoul <cn_yhn@sina.com>
2016-12-26 17:06:23 +0800

    添加基本样式统计

---
+++ js/input.js
@@ -0,0 +1,10 @@
+angular.module('dashboard',[])
+.controller('dashboardContrl',function($scope){
+	$scope.cashFlow = 2-3;
+	$scope.netAsset = -12000000.11;
+	$scope.passiveIncome = 12313;
+	$scope.cash = -500000.23;
+	$scope.balanceOfPayment = "12.3";
+	$scope.balanceOfSafety = "1";
+	
+})
