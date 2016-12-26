8cf50c95272860f3e594eb93396746abff3bf132
turingsoul <cn_yhn@sina.com>
2016-12-26 18:30:44 +0800

    内容样式

--- js/input.js
+++ js/input.js
@@ -6,5 +6,46 @@ angular.module('dashboard',[])
 	$scope.cash = -500000.23;
 	$scope.balanceOfPayment = "12.3";
 	$scope.balanceOfSafety = "1";
-	
+	$scope.currentChooseButton = "input";
+	$scope.inputClick = function(){
+		$scope.currentChooseButton = "input";
+		
+	}
+	$scope.outputClick = function(){
+		$scope.currentChooseButton = "output";
+	}
+	$scope.assetClick = function(){
+		$scope.currentChooseButton = "asset";
+	}
+	$scope.debtClick = function(){
+		$scope.currentChooseButton = "debt";
+	}
+	$scope.inputJudge = function(){
+		if($scope.currentChooseButton=='input'){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	$scope.outputJudge = function(){
+		if($scope.currentChooseButton=='output'){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	$scope.assetJudge = function(){
+		if($scope.currentChooseButton=='asset'){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	$scope.debtJudge = function(){
+		if($scope.currentChooseButton=='debt'){
+			return true;
+		}else{
+			return false;
+		}
+	}
 })
